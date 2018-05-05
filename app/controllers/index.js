3cc6ba834ea2fe25baae2c26cdd89048e501a6ed
Guilherme Araujo <guiluis.aa@gmail.com>
2018-05-05 19:57:39 -0300

    Fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -18,39 +18,37 @@ export default Controller.extend({
 
     searchBooks() {
 
-      if (this.get('searchValue') !== '') {
-        /**
-         * @desc Aqui eu "seto" a propriedade para dar o efeito de loading no botão e deixá-ço disabled
-         */
-        this.set('isSearching', true);
+      /**
+       * @desc Aqui eu "seto" a propriedade para dar o efeito de loading no botão e deixá-ço disabled
+       */
+      this.set('isSearching', true);
 
-        /**
-         * @desc Ajax para a API
-         */
-        $.ajax({
-          type: "GET",
-          url: `https://www.googleapis.com/books/v1/volumes?q=isbn+${this.get('searchValue')}`,
-  
-          headers: {
-            'API_KEY': 'AIzaSyBqKp73iHowC9fz4lqKT6avFSbNtdYSaK8'
-          }
-        })
-          .then(books =>  {
-            /**
-             * @desc No ember é necessário usar o método run() quando precisamos ter acesso ao this em um callback ou promisse se não for de um EmberObject
-             */
-            run(() => {
-              this.set('isSearching', false);
-              this.set('searchResults', books.items);
-            });
+      /**
+       * @desc Ajax para a API
+       */
+      $.ajax({
+        type: "GET",
+        url: `https://www.googleapis.com/books/v1/volumes?q=isbn+${this.get('searchValue')}`,
 
-          })
-          .catch(() => {
-            run(() => {
-              this.set('isSearching', false);
-            });
+        headers: {
+          'API_KEY': 'AIzaSyBqKp73iHowC9fz4lqKT6avFSbNtdYSaK8'
+        }
+      })
+        .then(books =>  {
+          /**
+           * @desc No ember é necessário usar o método run() quando precisamos ter acesso ao this em um callback ou promisse se não for de um EmberObject
+           */
+          run(() => {
+            this.set('isSearching', false);
+            this.set('searchResults', books.items);
           });
-      }
+
+        })
+        .catch(() => {
+          run(() => {
+            this.set('isSearching', false);
+          });
+        });
     },
 
     saveBook() {
