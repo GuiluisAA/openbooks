084b6cf4ba687e7a099ec711580a27a82a7e871f
Guilherme Araujo <guiluis.aa@gmail.com>
2018-05-05 17:47:10 -0300

    Google books API integration

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,54 @@
 import Controller from '@ember/controller';
+import { run } from '@ember/runloop';
+import $ from 'jquery';
 
 export default Controller.extend({
+
+  searchValue: null,
+  isSearching: false,
+
+  actions: {
+
+    searchBooks() {
+
+      if (this.get('searchValue') !== '') {
+        /**
+         * @desc Aqui eu "seto" a propriedade para dar o efeito de loading no botão e deixá-ço disabled
+         */
+        this.set('isSearching', true);
+
+        /**
+         * @desc Ajax para a API
+         */
+        $.ajax({
+          type: "GET",
+          url: `https://www.googleapis.com/books/v1/volumes?q=isbn+${this.get('searchValue')}`,
+  
+          headers: {
+            'API_KEY': 'AIzaSyBqKp73iHowC9fz4lqKT6avFSbNtdYSaK8'
+          }
+        })
+          .then(books =>  {
+            /**
+             * @desc No ember é necessário usar o método run() quando precisamos ter acesso ao this em um callback ou promisse se não for de um EmberObject
+             */
+            run(() => {
+              this.set('isSearching', false);
+              this.set('searchResults', books.items);
+            });
+
+          })
+          .catch(() => {
+            run(() => {
+              this.set('isSearching', false);
+            });
+          });
+      }
+    },
+
+    saveBook() {
+      console.log('Save Book');
+    }
+  }
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('searchResults', []);
+  }
+
 });
